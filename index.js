eab1cf1c78a9a77023288df90d7de4e1a6baeddd
fatema-nasrin <nasrinfatema35@gmail.com>
2021-08-05 19:35:32 +0400

    javascript practise

---
+++ index.js
@@ -0,0 +1,44 @@
+function handleProduct(isIncrise, product){
+    const input = document.getElementById(product + '-input');
+    const inputNum = parseInt(input.value);
+    let totalProduct = inputNum;
+    if (isIncrise === true){
+        totalProduct = inputNum + 1;
+    }
+    if (isIncrise === false && totalProduct > 0){
+        totalProduct = inputNum - 1;
+    }
+
+    document.getElementById(product + '-input').value = totalProduct;
+
+    let totalPrice = 0;
+
+    if (product === 'phn'){
+        totalPrice = 1399 * totalProduct;
+    }
+    if (product === 'case'){
+        totalPrice = 75 * totalProduct;
+    }
+    
+    document.getElementById(product + '-price').innerText = totalPrice;
+
+    NetTotal();
+}
+
+
+function NetTotal(){
+    const totalPhonePrice = document.getElementById('phn-price').innerText;
+    const totalPhonePriceNumber = parseInt(totalPhonePrice);
+
+    const totalCaseingPrice = document.getElementById('case-price').innerText;
+    const totalCaseingPriceNumber = parseInt(totalCaseingPrice);
+
+    const total =  totalPhonePriceNumber + totalCaseingPriceNumber;
+    document.getElementById('total').innerText = total;
+
+    const tax = Math.floor(total * 0.1); //10% tax rate
+    document.getElementById('tax-amount').innerText = tax;
+
+    const subtotal = total + tax;
+    document.getElementById('grand-total').innerText = subtotal;
+}
